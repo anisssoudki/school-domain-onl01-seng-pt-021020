e38eb06897928eaa2350937dc2c6fe81700c163c
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-19 19:53:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,8 +14,10 @@ class School
 # end
  
  def add_student(student, grade)
-   @roster[grade] = []
-   @roster[grade] << student
+   if roster.has_key?(grade)
+   roster[grade] << student
+ else
+   roster[grade] = [name]
  end
    
    
