64fb10a59aa12f86adf34b39735084a633986116
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-19 19:30:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+
+class School
+  attr_reader :name
+ def initialize(name)
+   
+   @name = name
+   @roster = {}
+ end
+ 
+ def roster
+   @roster
+ end
+ 
+ def add_student(student, grade)
+  
+   @roster[grade] = [name]
+   self.roster[@grade] << @names
+   
+ end
+   
+   
+
+ 
+ 
+end
